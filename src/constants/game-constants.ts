75b3f85a6fd9833c23d173051095811f7360ef73
Josh Beard <josh@jlbeard.com>
2020-10-11 00:49:57 -0400

    Got sockets connecting

--- src/constants/game-constants.ts
+++ src/constants/game-constants.ts
@@ -1,4 +1,8 @@
 export class GameConstants {
     public static WindowHeight: number = 600;
     public static WindowWidth: number = 800;
+
+    public static SocketServerAddress = "ws://0.0.0.0:5000/";
+    public static SocketServerRetryCount = 3;
+    public static SocketServerSendInterval = 20;
 }
--- src/web-worker/network-worker.js
+++ src/web-worker/network-worker.js
@@ -1,8 +1,11 @@
-const serverAddress = "localhost";
-const serverPort = 7777;
+var constants = require('../constants/game-constants');
 
-var client;
-var peer;
+var connection;
+var connected = false;
+var outboundMessageQueue = [];
+var inboundMessageQueue = [];
+var outboundMessageInterval;
+var playerId = "";
 
 self.addEventListener('message', function(e) {
     var data = e.data;
@@ -11,12 +14,86 @@ self.addEventListener('message', function(e) {
 
     switch (data.cmd) {
         case 'initialize':
-            
             createWorker();
             break;
     }
 }, false);
 
 function createWorker() {
+    createConnection();    
 
+    connection.onopen = (e) => {
+        console.log(`Connected to websocket server at ${connection.url}`);
+        connected = true;
+
+        sendInitialization();
+
+        outboundMessageInterval = setInterval(
+            processOutboundMessages, 
+            constants.GameConstants.SocketServerSendInterval);
+    };
+
+    connection.onclose = (e) => {
+        clearInterval(outboundMessageInterval);
+        console.log(`Connection to websocket server closed.`);
+        connected = false;
+
+        console.log(`Retrying server connection...`);
+        createConnection();
+    }
+
+    connection.onmessage = (e) => {
+        var message = JSON.parse(e.data);
+
+        if (Array.isArray(message)) {
+            console.log(`Received ${message.length} websocket messages`);
+
+            for (var i = 0; i < message.length; i++) {
+                processInboundMessage(message[i]);
+            }
+        } else {
+            console.log(`Received single websocket message of type ${message.messageType}`);
+            processInboundMessage(message);
+        }
+    }
+}
+
+function processInboundMessage(message) {
+    switch (message.messageType) {
+        case "initialization":
+            console.log("Received echo init");
+            break;
+        case "playerid":
+            playerId = message.messageData;
+            console.log(`PlayerId set to ${playerId}`);
+            break;
+    }
+}
+
+function createConnection() {
+    connection = new WebSocket(constants.GameConstants.SocketServerAddress);
+    connection.binaryType = "arraybuffer";
+}
+
+function sendInitialization() {
+    outboundMessageQueue.push({
+        "messageType": "initialization",
+        "messageData": ""
+    });
+}
+
+function processOutboundMessages() {
+    var messagesToSend = [];
+
+    while (outboundMessageQueue.length > 0) {
+        messagesToSend.push(outboundMessageQueue.shift());
+    }
+
+    if (messagesToSend.length > 0) {
+        var convertedData = JSON.stringify(messagesToSend);
+
+        console.log(`Sending ${messagesToSend.length} websocket messages`);
+    
+        connection.send(convertedData);
+    }
 }
